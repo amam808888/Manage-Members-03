c69a0c97e069e566fb16137bf4b2a0a722b69231
rittidetnpk1 <rittidetnpk1@gmail.com>
2025-05-04 15:26:40 +0700

    [Update] Insert, Delete, Get Member Data from MongoDB

---
+++ database.js
@@ -0,0 +1,85 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = "mongodb+srv://rittidetnpk2:D9dIMpjTbf723mWb@manage-member.oz8jjul.mongodb.net/?retryWrites=true&w=majority&appName=Manage-Member";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+const db = client.db('Manage-Member')
+const members = db.collection('members')
+
+const insertMember = async ({ username, password, securityCode, expiryDate }) => {
+    try {
+        await client.connect();
+        const data = {
+            username, 
+            password, 
+            securityCode, 
+            expiryDate
+        }
+        const result = await members.insertOne(data)
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } catch (e) {
+        console.warn(e)
+    } finally {
+        await client.close()
+    }
+}
+
+const deleteMember = async (id) => {
+    try {
+        await client.connect();
+        const query = {
+            _id: new ObjectId(id)
+        }
+        const result = await members.deleteOne(query)
+        if (result.deletedCount === 1) {
+            console.log("Successfully deleted one document.");
+          } else {
+            console.log("No documents matched the query. Deleted 0 documents.");
+          }
+    } catch (e) {
+        console.warn(e)
+    } finally {
+        await client.close()
+    }
+}
+
+const getMembers = async () => {
+    try {
+        await client.connect();
+        const result = await members.find().toArray()
+        return result
+    } catch (e) {
+        console.warn(e)
+    } finally {
+        await client.close()
+    }
+}
+
+module.exports = {
+    insertMember,
+    deleteMember,
+    getMembers
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs');
+const cors = require('cors')
+const database = require('./database')
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json()
+
+app.use(cors())
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('Whoops! File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+})
+
+app.post('/', jsonParser, async (req, res) => {
+    await database.insertMember(req.body)
+    res.status(200)
+    res.send({ status: 'OK' })
+})
+
+app.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    await database.deleteMember(id)
+    res.status(200)
+    res.send({ status: 'OK' })
+})
+
+app.get('/getMembers', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    const members = await database.getMembers()
+    res.json(members)
+})
